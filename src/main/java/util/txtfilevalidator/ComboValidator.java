224dd6d4297f8f286083e84ff738abc21b8e46b9
Matyshenko <eugene.matushenko@gmail.com>
2019-03-04 22:43:35 +0300

    version 2.0

---
+++ src/main/java/util/txtfilevalidator/ComboValidator.java
@@ -0,0 +1,10 @@
+package util.txtfilevalidator;
+
+import java.io.File;
+
+public class ComboValidator implements TxtValidatable {
+    @Override
+    public boolean validateFile(File file) {
+        return false;
+    }
+}
